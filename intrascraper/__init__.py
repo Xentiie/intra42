b0469046706d8109dbc5ef7e5621e93b0dec24a5
Rémi Claire <reclaire@student.42mulhouse.fr>
2023-05-30 23:00:27 +0200

    cluster map

--- intrascraper/__init__.py
+++ intrascraper/__init__.py
@@ -3,4 +3,5 @@ from .intrascraper import (get_all_notifs,
                            get_ssh_key,
                            delete_ssh_key,
                            set_ssh_key,
-                           get_location_data)
+                           get_location_data,
+                           get_clusters_map)
